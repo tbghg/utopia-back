2020295d37cc688690a4cd9445fdfacaf7d91c84
gallifrey <956465331@qq.com>
2023-10-27 13:48:33 +0800

    fix: redis连接问题

--- pkg/redis/init.go
+++ pkg/redis/init.go
@@ -3,7 +3,8 @@ package redis
 import (
 	"context"
 	"github.com/redis/go-redis/v9"
-	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"utopia-back/config"
 	"utopia-back/pkg/logger"
 )
 
@@ -11,19 +12,18 @@ var RDB *redis.Client
 
 func Init() {
 	ctx := context.Background()
-	viper.AddConfigPath("config")
-	viper.SetConfigName("config")
+
 	RDB = redis.NewClient(&redis.Options{
-		Addr:     viper.GetString("redis.addr"),
-		Password: viper.GetString("redis.password"),
-		DB:       viper.GetInt("redis.db"),
+		Addr:     config.V.GetString("redis.addr"),
+		Password: config.V.GetString("redis.password"),
+		DB:       config.V.GetInt("redis.db"),
 	})
 	_, err := RDB.Ping(ctx).Result()
 	if err != nil {
-		logger.Logger.Error("redis connect failed")
+		logger.Logger.Error("redis连接失败", zap.String("err", err.Error()))
 		return
 	}
 
-	logger.Logger.Info("redis connect success")
+	logger.Logger.Info("redis连接成功")
 
 }
