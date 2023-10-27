6ca68b5e4524cd1498379dbf36799d3a07810267
冰航 <tianbinghang@bilibili.com>
2023-10-27 19:26:42 +0800

    fix: 修复上传视频回调接口

--- http/controller/v1/video.go
+++ http/controller/v1/video.go
@@ -1,8 +1,11 @@
 package v1
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/qiniu/go-sdk/v7/auth/qbox"
 	"net/http"
+	"strconv"
 	"utopia-back/config"
 	utils "utopia-back/pkg/util"
 	"utopia-back/service/abstract"
@@ -22,15 +25,18 @@ type uploadVideoTokenData struct {
 
 type uploadCallbackReq struct {
 	Key      string `json:"key" validate:"required"`
-	IsImage  bool   `json:"is_image"`
-	AuthorId uint   `json:"author_id" `
-	CoverUrl string `json:"cover_url" `
+	IsImage  string `json:"is_image"`
+	AuthorId string `json:"author_id"` // todo 可更改为JWT-Token，增强安全性
+	CoverUrl string `json:"cover_url"`
 	Describe string `json:"describe"`
 }
 
 type uploadCallbackData struct {
 	ImageUrl string `json:"image_url"`
 }
+
+// 上传是否为图片
+const callbackIsImage = "YES"
 
 func (v *VideoController) UploadVideoToken(c *gin.Context) {
 	upToken := utils.GetCallbackToken()
@@ -58,10 +64,16 @@ func (v *VideoController) UploadVideoCallback(c *gin.Context) {
 			})
 		}
 	}()
-	// todo 校验是否为七牛云发送
-
+	// 校验是否为七牛云调用
+	isQiNiu, err := qbox.VerifyCallback(utils.GetMac(), c.Request)
+	if !isQiNiu || err != nil {
+		if err == nil {
+			err = errors.New("非七牛云服务发送")
+		}
+		return
+	}
 	// 接收参数并绑定
-	if err = c.ShouldBindJSON(&r); err != nil {
+	if err = c.BindJSON(&r); err != nil {
 		return
 	}
 	// 参数校验
@@ -69,8 +81,8 @@ func (v *VideoController) UploadVideoCallback(c *gin.Context) {
 		return
 	}
 
-	url := config.V.GetString("qiniu.kodoApi") + r.Key
-	if r.IsImage {
+	url := config.V.GetString("qiniu.kodoApi") + "/" + r.Key
+	if r.IsImage == callbackIsImage {
 		c.JSON(http.StatusOK, &ResponseWithData{
 			Code: SuccessCode,
 			Msg:  "ok",
@@ -80,8 +92,8 @@ func (v *VideoController) UploadVideoCallback(c *gin.Context) {
 		})
 		return
 	}
-
-	err = v.Service.UploadVideoCallback(r.AuthorId, url, r.CoverUrl, r.Describe)
+	authorId, _ := strconv.ParseUint(r.AuthorId, 10, 64)
+	err = v.Service.UploadVideoCallback(uint(authorId), url, r.CoverUrl, r.Describe)
 	if err != nil {
 		return
 	}
