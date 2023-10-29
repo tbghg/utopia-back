0c29a0f577af707e5a41752ee29c0d39ddc3c4d3
gallifrey <956465331@qq.com>
2023-10-29 18:18:54 +0800

    style: 添加一些注释

--- initialize/ratelimit.go
+++ initialize/ratelimit.go
@@ -11,14 +11,15 @@ func InitRateLimit() {
 	// 需要限流的接口在此注册
 	// 参考文章：https://blog.csdn.net/m0_52528053/article/details/127294249
 	middleware.BucketMap["/api/v1/upload/token"] = &middleware.BucketConf{
-		Bucket:  ratelimit.NewBucketWithQuantum(10*time.Millisecond, 10, 5),
+		// 每10ms产生5个token，最多存储10个token
+		Bucket: ratelimit.NewBucketWithQuantum(10*time.Millisecond, 10, 5),
+		// 最大等待时间
 		MaxWait: 20 * time.Millisecond,
 	}
 	middleware.BucketMap["/api/v1/upload/callback"] = &middleware.BucketConf{
 		Bucket:  ratelimit.NewBucketWithQuantum(10*time.Millisecond, 10, 5),
 		MaxWait: 20 * time.Millisecond,
 	}
-
 	middleware.BucketMap["default"] = &middleware.BucketConf{
 		Bucket:  ratelimit.NewBucketWithQuantum(10*time.Millisecond, 100, 20),
 		MaxWait: 10 * time.Millisecond,
