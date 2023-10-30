315c5cae317d51e12d6355c99f236f0fb77c56c7
冰航 <tianbinghang@bilibili.com>
2023-10-30 17:34:56 +0800

    fix: 修改Video字段类型

--- model/video.go
+++ model/video.go
@@ -15,10 +15,10 @@ type Video struct {
 	DeletedAt time.Time `gorm:"index:idx_author_del,priority:2;index:idx_ctime_del,priority:2"`
 
 	AuthorID    uint   `gorm:"not null;index:idx_author_del,priority:1" json:"author_id"` // 作者id
-	PlayUrl     string `gorm:"type:varchar(64);not null" json:"play_url"`                 // 视频播放地址
-	CoverUrl    string `gorm:"type:varchar(64);not null" json:"cover_url"`                // 视频封面
+	PlayUrl     string `gorm:"type:varchar(256);not null" json:"play_url"`                // 视频播放地址
+	CoverUrl    string `gorm:"type:varchar(256);not null" json:"cover_url"`               // 视频封面
 	VideoTypeID uint   `gorm:"not null" json:"video_type_id"`                             // 视频类型
-	Describe    string `gorm:"type:varchar(64)" json:"describe"`                          // 描述
+	Describe    string `gorm:"type:varchar(256)" json:"describe"`                         // 描述
 }
 
 // VideoInfo 视频信息
