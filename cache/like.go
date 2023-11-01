070b5225e356b35d5b64cf223be897b6ac48177d
冰航 <tianbinghang@bilibili.com>
2023-11-01 17:13:19 +0800

    fix: 修改点赞缓存

--- cache/like.go
+++ cache/like.go
@@ -10,10 +10,12 @@ import (
 )
 
 const (
-	sMinVid     = "minVid"
-	sTTL        = "ttl"
-	maxFieldNum = 1500 // hash中最大字段数
-	setFieldNum = 750  // hash重设字段数
+	sMinVid                        = "minVid"
+	sTTL                           = "ttl"
+	maxFieldNum                    = 1500 // hash中最大字段数
+	setFieldNum                    = 750  // hash重设字段数
+	userLikedVideoExpireTime       = time.Hour * 24 * 15
+	userLikedVidoeUpdateExpireTime = time.Hour * 24 * 15 / 3
 )
 
 // VideoLikeCountKey 视频点赞数
@@ -35,49 +37,49 @@ func UserLikedVideoKeyV3(uid uint) string {
 
 // IsUserLikedVideo 用户是否为某个视频点赞(单个)
 //
-// state 1 查询成功；2 不存在该key,需要新建；3 冷数据,需回源
+// state 0 查询成功；1 不存在该key,需要新建；2 冷数据,需回源；3 查询失败，需要回源
 func IsUserLikedVideo(uid uint, vid uint) (liked bool, state int) {
 	key := UserLikedVideoKeyV3(uid)
 
 	// 查询是否存在
-	res := RDB.HMGet(Ctx, key, sMinVid, sTTL, strconv.Itoa(int(vid)))
-	if res.Err() != nil {
-		logger.Logger.Error(fmt.Sprintf("IsUserLikedVideo cmd:%v", res.String()))
+	resHMGet := RDB.HMGet(Ctx, key, sMinVid, sTTL, strconv.Itoa(int(vid)))
+	if resHMGet.Err() != nil {
+		logger.Logger.Error(fmt.Sprintf("IsUserLikedVideo cmd:%v", resHMGet.String()))
+		state = 3
 		return
 	} else {
-		logger.Logger.Info(fmt.Sprintf("IsUserLikedVideo cmd:%v", res.String()))
+		logger.Logger.Info(fmt.Sprintf("IsUserLikedVideo cmd:%v", resHMGet.String()))
 	}
 
-	minVid, ok1 := res.Val()[0].(uint)
-	ttl, ok2 := res.Val()[1].(time.Time)
-
+	sMinVidVal, ok1 := resHMGet.Val()[0].(string)
+	sTtlVal, ok2 := resHMGet.Val()[1].(string)
 	if !ok1 || !ok2 { // 不存在该key，需重新构建
-		state = 2
+		state = 1
 		return
 	}
 
+	minVid, _ := strconv.Atoi(sMinVidVal)
+	ttl, _ := strconv.Atoi(sTtlVal)
+
 	// 剩余时间小于三分之一，重新续期
-	if time.Now().Sub(ttl) < time.Hour*24*15/3 {
-		res := RDB.Expire(Ctx, key, time.Hour*24*15)
-		if err := res.Err(); err != nil {
-			logger.Logger.Error(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", res.String()))
+	// 此处失败不影响服务，打印日志即可，不用向上层传递
+	if time.Unix(int64(ttl), 0).Sub(time.Now()) < time.Hour*24*15/3 {
+		resExpire := RDB.Expire(Ctx, key, time.Hour*24*15)
+		if err := resExpire.Err(); err != nil {
+			logger.Logger.Error(fmt.Sprintf("IsUserLikedVideo RDB.Expire cmd:%v", resExpire.String()))
 		} else {
-			logger.Logger.Info(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", res.String()))
+			logger.Logger.Info(fmt.Sprintf("IsUserLikedVideo RDB.Expire cmd:%v", resExpire.String()))
 		}
 	}
 
-	_, ok := res.Val()[2].(uint)
-	// 如果不是冷数据，此时就是最终结果
-	if vid >= minVid {
-		state = 1
-		liked = ok
+	if resHMGet.Val()[2] != nil {
+		liked = true
 		return
 	}
-	// 冷数据，查到则返回true，未查到设置state
-	if ok {
-		liked = true
-	} else {
-		state = 3
+	// 判断是否为冷数据
+	if vid < uint(minVid) {
+		state = 2
+		return
 	}
 	return
 }
@@ -85,69 +87,55 @@ func IsUserLikedVideo(uid uint, vid uint) (liked bool, state int) {
 // IsUserLikedVideos 用户是否为某些视频点赞(批量)
 //
 // result key为vid  <->  0未点赞；1为点赞；2 冷数据,需回源
-// state 1 成功；2 不存在该key,需要新建
+// state 0 成功；1 不存在该key,需要新建；2 查询失败,需回源
 func IsUserLikedVideos(uid uint, videoIds []uint) (result map[uint]int, state int) {
 	key := UserLikedVideoKeyV3(uid)
 
-	var builder strings.Builder
-	for i := range videoIds {
-		builder.WriteString(strconv.Itoa(int(videoIds[i])))
-		builder.WriteString(" ")
+	fields := make([]string, len(videoIds)+2)
+	fields[0], fields[1] = sMinVid, sTTL
+	// 将整数切片转换为字符串切片
+	for i, num := range videoIds {
+		fields[i+2] = strconv.FormatInt(int64(num), 10)
 	}
-	field := builder.String()
 
 	// 查询是否存在
-	res := RDB.HMGet(Ctx, key, sMinVid, sTTL, field)
-	if res.Err() != nil {
-		logger.Logger.Error(fmt.Sprintf("IsUserLikedVideo cmd:%v", res.String()))
-		return
-	} else {
-		logger.Logger.Info(fmt.Sprintf("IsUserLikedVideo cmd:%v", res.String()))
-	}
-
-	minVid, ok1 := res.Val()[0].(uint)
-	ttl, ok2 := res.Val()[1].(time.Time)
-
-	if !ok1 || !ok2 { // 不存在该key，需重新构建
+	resHMGet := RDB.HMGet(Ctx, key, fields...)
+	if resHMGet.Err() != nil {
+		logger.Logger.Error(fmt.Sprintf("IsUserLikedVideos cmd:%v", resHMGet.String()))
 		state = 2
 		return
-	}
-
-	state = 1
-
-	// 判断数量是否超过域值，超过则重建
-	lenRes := RDB.HLen(Ctx, key)
-	if res.Err() != nil {
-		logger.Logger.Error(fmt.Sprintf("IsUserLikedVideo RDB.HLen cmd:%v", lenRes.String()))
-		return
 	} else {
-		logger.Logger.Info(fmt.Sprintf("IsUserLikedVideo RDB.HLen cmd:%v", lenRes.String()))
+		logger.Logger.Info(fmt.Sprintf("IsUserLikedVideos cmd:%v", resHMGet.String()))
 	}
 
-	if lenRes.Val() > maxFieldNum { // 数量超过域值
-		go rebuildUserLikedVideos(key)
-
-	} else if time.Now().Sub(ttl) < time.Hour*24*15/3 { // 剩余时间小于三分之一，重新续期
-
-		res := RDB.Expire(Ctx, key, time.Hour*24*15)
-		if err := res.Err(); err != nil {
-			logger.Logger.Error(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", res.String()))
-		} else {
-			logger.Logger.Info(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", res.String()))
-		}
+	sMinVidVal, ok1 := resHMGet.Val()[0].(string)
+	sTtlVal, ok2 := resHMGet.Val()[1].(string)
+	if !ok1 || !ok2 { // 不存在该key，需重新构建
+		state = 1
+		return
 	}
 
-	vidRes := res.Val()[2:]
+	minVid, _ := strconv.Atoi(sMinVidVal)
+	ttl, _ := strconv.Atoi(sTtlVal)
+
+	state = 0 // 查询成功，state标为0
+
+	vidRes := resHMGet.Val()[2:]
 	result = make(map[uint]int, len(videoIds))
+
 	for i, vid := range videoIds {
-		if _, ok := vidRes[i].(uint); ok { // 查询到点赞
+		if _, ok := vidRes[i].(string); ok { // 查询到点赞
 			result[vid] = 1
-		} else if vid >= minVid { // 热数据，用户没点赞
+		} else if vid >= uint(minVid) { // 热数据，用户没点赞
 			result[vid] = 0
 		} else { // 冷数据，需要回源
 			result[vid] = 2
 		}
 	}
+
+	// 判断是否超过域值，是否需要续期
+	judgeRebuildVideoLikedVideos(key, ttl)
+
 	return
 }
 
@@ -155,46 +143,94 @@ func IsUserLikedVideos(uid uint, videoIds []uint) (result map[uint]int, state in
 func BuildUserLikedVideos(key string, vid []uint, minVid uint) {
 	var err error
 
-	ttl := time.Now().AddDate(0, 0, 15) // 一个月的过期时间
+	ttl := time.Now().AddDate(0, 0, 15).Unix() // 一个月的过期时间
 
-	var builder strings.Builder
-	for i := range vid {
-		builder.WriteString(strconv.Itoa(int(vid[i])))
-		builder.WriteString(" 1 ")
-	}
-	field := builder.String()
-
-	res := RDB.HMSet(Ctx, key, sTTL, ttl, sMinVid, minVid, field)
-	if err = res.Err(); err != nil {
-		logger.Logger.Error(fmt.Sprintf("BuildUserLikedVideos RDB.HMSet cmd:%v", res.String()))
-	} else {
-		logger.Logger.Info(fmt.Sprintf("BuildUserLikedVideos RDB.HMSet cmd:%v", res.String()))
+	fields := make(map[string]interface{})
+	fields[sMinVid] = minVid
+	fields[sTTL] = ttl
+	for _, v := range vid {
+		fields[strconv.Itoa(int(v))] = true
 	}
 
-	res = RDB.Expire(Ctx, key, time.Hour*24*15)
-	if err = res.Err(); err != nil {
-		logger.Logger.Error(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", res.String()))
+	resHSet := RDB.HSet(Ctx, key, fields)
+	if err = resHSet.Err(); err != nil {
+		logger.Logger.Error(fmt.Sprintf("BuildUserLikedVideos RDB.HMSet cmd:%v", resHSet.String()))
 	} else {
-		logger.Logger.Info(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", res.String()))
+		logger.Logger.Info(fmt.Sprintf("BuildUserLikedVideos RDB.HMSet cmd:%v", resHSet.String()))
+	}
+
+	resExpire := RDB.Expire(Ctx, key, time.Hour*24*15)
+	if err = resExpire.Err(); err != nil {
+		logger.Logger.Error(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", resExpire.String()))
+	} else {
+		logger.Logger.Info(fmt.Sprintf("BuildUserLikedVideos RDB.Expire cmd:%v", resExpire.String()))
 	}
 }
 
 func rebuildUserLikedVideos(key string) {
-	res := RDB.HGetAll(Ctx, key)
-	if err := res.Err(); err != nil {
-		logger.Logger.Error(fmt.Sprintf("rebuildUserLikedVideos RDB.HGetAll cmd:%v", res.String()))
+	resHGetAll := RDB.HGetAll(Ctx, key)
+	if err := resHGetAll.Err(); err != nil {
+		logger.Logger.Error(fmt.Sprintf("rebuildUserLikedVideos RDB.HGetAll cmd:%v", resHGetAll.String()))
 	} else {
-		logger.Logger.Info(fmt.Sprintf("rebuildUserLikedVideos RDB.HGetAll cmd:%v", res.String()))
+		logger.Logger.Info(fmt.Sprintf("rebuildUserLikedVideos RDB.HGetAll cmd:%v", resHGetAll.String()))
 	}
 
-	videoIds := make([]uint, 0, len(res.Val()))
-	for k := range res.Val() {
-		num, _ := strconv.Atoi(k)
-		videoIds = append(videoIds, uint(num))
+	videoIds := make([]uint, 0, len(resHGetAll.Val())-2)
+	for k := range resHGetAll.Val() {
+		if num, err := strconv.Atoi(k); err == nil {
+			videoIds = append(videoIds, uint(num))
+		}
 	}
+
 	sort.Slice(videoIds, func(i, j int) bool {
 		return videoIds[i] > videoIds[j]
 	})
 
-	BuildUserLikedVideos(key, videoIds[:setFieldNum], videoIds[setFieldNum-1])
+	if len(videoIds) < maxFieldNum {
+		logger.Logger.Info(fmt.Sprintf("rebuildUserLikedVideos len(videoIds):%v < maxFieldNum:%v 不需要重建", len(videoIds), maxFieldNum))
+		return
+	}
+
+	BuildUserLikedVideos(key, nil, videoIds[setFieldNum-1])
+
+	var builder strings.Builder
+	lastVids := videoIds[setFieldNum:]
+	for i, v := range lastVids {
+		builder.WriteString(strconv.Itoa(int(v)))
+		if i != len(lastVids)-1 {
+			builder.WriteString(" ")
+		}
+	}
+	fields := builder.String()
+
+	resDel := RDB.HDel(Ctx, key, fields)
+	if err := resDel.Err(); err != nil {
+		logger.Logger.Error(fmt.Sprintf("rebuildUserLikedVideos RDB.HDel cmd:%v", resDel.String()))
+	} else {
+		logger.Logger.Info(fmt.Sprintf("rebuildUserLikedVideos RDB.HDel cmd:%v", resDel.String()))
+	}
+}
+
+// 判断数量是否超过域值，是否需要续期
+// 此处失败不影响服务，打印日志即可，不用向上层传递
+func judgeRebuildVideoLikedVideos(key string, ttl int) {
+	resLen := RDB.HLen(Ctx, key)
+	if resLen.Err() != nil {
+		logger.Logger.Error(fmt.Sprintf("judgeRebuildVideoLikedVideos RDB.HLen cmd:%v", resLen.String()))
+	} else {
+		logger.Logger.Info(fmt.Sprintf("judgeRebuildVideoLikedVideos RDB.HLen cmd:%v", resLen.String()))
+	}
+
+	if resLen.Val() > maxFieldNum { // 数量超过域值
+		go rebuildUserLikedVideos(key)
+
+	} else if time.Unix(int64(ttl), 0).Sub(time.Now()) < userLikedVidoeUpdateExpireTime { // 剩余时间小于三分之一，重新续期
+
+		resExpire := RDB.Expire(Ctx, key, userLikedVideoExpireTime)
+		if err := resExpire.Err(); err != nil {
+			logger.Logger.Error(fmt.Sprintf("judgeRebuildVideoLikedVideos RDB.Expire cmd:%v", resExpire.String()))
+		} else {
+			logger.Logger.Info(fmt.Sprintf("judgeRebuildVideoLikedVideos RDB.Expire cmd:%v", resExpire.String()))
+		}
+	}
 }
